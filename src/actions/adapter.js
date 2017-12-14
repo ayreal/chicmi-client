bb7bf8ee00c2c6add2dcd3a6c38118ca68beb371
Ariel Herman <aeh.herman@gmail.com>
2017-12-14 11:06:00 -0500

    correct endpoint in adapter

--- src/actions/adapter.js
+++ src/actions/adapter.js
@@ -1,17 +1,17 @@
 // const ROUTE = "https://randomuser.me/api/";
-const ROUTE = "http://localhost:3000";
+const ROUTE = "http://localhost:3000/api/v1";
 
 const headers = {
   Accepts: "application/json, text/plain",
   "Content-Type": "application/json"
 };
 
-export function getUser() {
-  return fetch("https://randomuser.me/api/").then(res => res.json());
-}
+// export function getUser() {
+//   return fetch("https://randomuser.me/api/").then(res => res.json());
+// }
 
 export function fetchUser(data) {
-  return fetch(`${ROUTE}/auth`, {
+  return fetch(`${ROUTE}/login`, {
     method: "POST",
     headers: headers,
     body: JSON.stringify(data)
