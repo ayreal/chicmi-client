1c3e57d05a24b7dc729281be9e9e05b7a395dda9
Ariel Herman <aeh.herman@gmail.com>
2017-12-14 19:20:37 -0500

    basic styling for top level components

--- src/containers/Events.js
+++ src/containers/Events.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions";
+import { Container, Header } from "semantic-ui-react";
 
 class Events extends Component {
   render() {
     console.log("%c Inside render containers/Events.js \n", "color: #bada55");
     console.log("----------------------- \n");
 
-    return <div>Inside Events</div>;
+    return (
+      <Container text style={{ marginTop: "7em" }}>
+        <Header as="h1">This Week's Sample Sales</Header>
+        <p>Some text here.</p>
+      </Container>
+    );
   }
 }
 
--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions";
+import { Container, Image, Menu } from "semantic-ui-react";
 
 class Navbar extends Component {
   render() {
     console.log("%c Inside render containers/Navbar.js \n", "color: #bada55");
     console.log("----------------------- \n");
 
-    return <div>Inside Navbar</div>;
+    return (
+      <Menu fixed="top" inverted>
+        <Container>
+          <Menu.Item as="a" header>
+            <Image
+              size="mini"
+              src="/logo.png"
+              style={{ marginRight: "1.5em" }}
+            />
+            ChicMi
+          </Menu.Item>
+          <Menu.Item as="a">Home</Menu.Item>
+        </Container>
+      </Menu>
+    );
   }
 }
 
