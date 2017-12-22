ca4bb93bb568f800e0d5a8b3143febf7305e1542
Ariel Herman <aeh.herman@gmail.com>
2017-12-22 14:07:47 -0500

    can click events on map

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -8,6 +8,29 @@ const style = {
   position: "relative"
 };
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  };
+
+  onMapClicked = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     console.log("%c Inside render MapContainer \n", "color: #bada55");
     console.log("PROPS: ", this.props);
@@ -23,6 +46,7 @@ export class MapContainer extends Component {
           lat: 40.7433809,
           lng: -73.9912899
         }}
+        onClick={this.onMapClicked}
       >
         {this.props.events.map(myEvent => (
           <Marker
@@ -33,9 +57,17 @@ export class MapContainer extends Component {
               lat: myEvent.latitude,
               lng: myEvent.longitude
             }}
+            onClick={this.onMarkerClick}
           />
         ))}
-        <InfoWindow onClose={this.onInfoWindowClose} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
       </Map>
     );
   }
