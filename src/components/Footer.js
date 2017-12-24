862682d29784a8cccf23c334c213b5392af2f859
Ariel Herman <aeh.herman@gmail.com>
2017-12-24 09:26:31 -0500

    add content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Container, Grid, Header, List, Segment } from "semantic-ui-react";
+import {
+  Container,
+  Grid,
+  Header,
+  List,
+  Segment,
+  Icon
+} from "semantic-ui-react";
 
 const Footer = props => (
   <Segment inverted vertical style={{ padding: "5em 0em" }}>
@@ -7,28 +14,34 @@ const Footer = props => (
       <Grid divided inverted stackable>
         <Grid.Row>
           <Grid.Column width={3}>
-            <Header inverted as="h4" content="About" />
+            <Header inverted as="h4" content="GET INVOLVED" />
             <List link inverted>
-              <List.Item as="a">Link</List.Item>
-              <List.Item as="a">Link</List.Item>
-              <List.Item as="a">Link</List.Item>
-              <List.Item as="a">Link</List.Item>
+              <List.Item as="a">Email Fashion Alerts</List.Item>
+              <List.Item as="a">Become a VIP</List.Item>
+              <List.Item as="a">Get the App</List.Item>
+              <List.Item as="a">List Your Event</List.Item>
+              <List.Item as="a">For Business</List.Item>
+              <List.Item as="a">Tools for Bloggers</List.Item>
             </List>
           </Grid.Column>
           <Grid.Column width={3}>
-            <Header inverted as="h4" content="Services" />
+            <Header inverted as="h4" content="ABOUT" />
             <List link inverted>
-              <List.Item as="a">Link</List.Item>
-              <List.Item as="a">Link</List.Item>
-              <List.Item as="a">Link</List.Item>
+              <List.Item as="a">Who We Are</List.Item>
+              <List.Item as="a">News</List.Item>
+              <List.Item as="a">Contact Us</List.Item>
               <List.Item as="a">Link</List.Item>
             </List>
           </Grid.Column>
           <Grid.Column width={7}>
             <Header as="h4" inverted>
-              More
+              FOLLOW US
             </Header>
-            <p>Stuff here.</p>
+            <Icon name="instagram" size="large" />
+            <Icon name="facebook" size="large" />
+            <Icon name="twitter" size="large" />
+            <Icon name="snapchat" size="large" />
+            <Icon name="google" size="large" />
           </Grid.Column>
         </Grid.Row>
       </Grid>
