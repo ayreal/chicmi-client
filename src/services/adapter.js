c14e978d73b19e1f4d00a93f5670b83b75bbf3a1
Ariel Herman <aeh.herman@gmail.com>
2017-12-28 20:39:55 -0500

    work on event slug fix

--- src/services/adapter.js
+++ src/services/adapter.js
@@ -63,6 +63,7 @@ export function fetchDeleteEvent(userId, eventId) {
 
 export function fetchEventBySlug(slug) {
   console.log("hit event by slug", slug);
+  debugger;
   return fetch(`${ROUTE}/get_event`, {
     method: "POST",
     headers: headers,
