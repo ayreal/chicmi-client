8f5d096028b69018e32132d94d0dda323836031d
Ariel Herman <aeh.herman@gmail.com>
2018-01-02 18:29:09 -0500

    style calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import BigCalendar from "react-big-calendar";
 import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./calendarStyles.css";
 
 BigCalendar.momentLocalizer(moment);
 
