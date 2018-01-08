a8cd42acc5eafbb5cba117484fb460f4ded86dd9
Ariel Herman <aeh.herman@gmail.com>
2018-01-08 09:54:51 -0500

    change route

--- src/services/adapter.js
+++ src/services/adapter.js
@@ -1,5 +1,5 @@
-// const ROUTE = "https://chicmi-api.herokuapp.com/api/v1";
-const ROUTE = "http://localhost:3000/api/v1";
+const ROUTE = "https://chicmi-api.herokuapp.com/api/v1";
+// const ROUTE = "http://localhost:3000/api/v1";
 
 const EXT_ROUTE =
   "https://www.chicmi.com/api/calendar_in_city/?city=new-york&types=sample-sales&sectors=&designers=&stores=&users=&featured_only=&max_results=&days=90&source=embed";
